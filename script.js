11bbc7214bd561d814c51e4137e8df11935d44d2
hassarennes <hassanrennes@gmail.com>
2016-07-04 03:26:42 +0200

    script prenant en compte la multiplication

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-var display = document.getElementById('display');
-var click = document.getElementById('click');
-var score = 0;
-function displayScore() {
-  display.innerHTML = score;
+$bouton = document.getElementById("bouton");
+$multiplicateur = document.getElementById("multiplicateur");
+$score = document.getElementById("score");
+score = 0;
+nbMultiplicateur = 1;
+
+function afficherScore() {
+    $score.innerHTML = "Score : " + score;
 }
-function increaseScore() {
-  score += clickValue;
-  buttonsEnabler();
-  displayScore();
+
+function afficherNbMultiplicateur() {
+    $multiplicateur.innerHTML = "Multiplicateur x" + nbMultiplicateur + " (prix du prochain : " + prix() + ")";
 }
+
+function clic() {
+    score = score + nbMultiplicateur;
+    afficherScore();
 }
-function autoclickF() {
-  if (autoclickOn) {
-    increaseScore();
-  }
-  displayScore();
-  click.addEventListener('click', increaseScore);
+
+function prix() {
+    return 20 * nbMultiplicateur * nbMultiplicateur;
+}
+
+function acheterMultiplicateur() {
+    if (score >= prix()) {
+        score = score - prix();
+        nbMultiplicateur = nbMultiplicateur + 1;
+        afficherNbMultiplicateur();
+        afficherScore();
+    } else {
+        alert("Votre score est insuffisant !");
+    }
+}
+
+$bouton.onclick = clic;
+$multiplicateur.onclick = acheterMultiplicateur;
+afficherScore();
+afficherNbMultiplicateur();
