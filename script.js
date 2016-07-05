6c9513df41d9052f6631803e874558e72053ff61
hassarennes <hassanrennes@gmail.com>
2016-07-05 02:38:52 +0200

    derniere modif et essaie de mise en forme

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
 
-var score = 0;
-var btn = document.getElementById("btn");
-btn.onclick = function () {
+var click = document.getElementById('click');
+  var score = 0;
+  var view = document.getElementById('view');
+  var multiplier = document.getElementById('multi');
+  var countClick = 1;
+  var prix = 20;
+  var autoclick = document.getElementById('autoclick');
+  
+  
 
-    if (click >= 1) {    Multiply
-        score = score + click;
-    } else {
-        score = score + 1;
-    }
-    document.getElementById("display").innerHTML = score;
-};
-var multiply = document.getElementById("multiply");
-var click = 0;
-multiply.onclick = function augmenterMultiplicateur() {
-    var price =  click * 50 ; multiplier
-    if (score > price || 50 ){  
-    click++;
-    if (price !== 0){ 
-    
- }else {
-   score = score - 50;   
- }
-     document.getElementById("display").innerHTML = score;
-     
-    }
-    multiply.innerHTML = "x"+ click;
-    
-};
+
+  var addScore = function(){
+     if (score >= 0) {
+         score = score + countClick;
+         view.innerHTML = score;
+         }
+
+  }
+  click.addEventListener("click", addScore);
+
+  var multiple = function(){
+
+
+     if (score >= prix) {
+         countClick = countClick * 2;
+         score = score - prix;
+         prix = prix * 2;
+         multiplier.value = "multiplier x" + countClick + " prochaine multiplication, prix " + prix;
+
+     }
+  }
+  multiplier.addEventListener("click", multiple);
+
+  var auto = function(){
+     if (score > 50){
+         score = score - 50;
+         setInterval(addScore,1000);
+     }
+     autoclick.style.display  = "none";
+  }
+  autoclick.addEventListener("click", auto);
+
+  
+
+
+  
